cbe184efeb06d5ceb2c43bcda2a5bf89d9760b88
Arthur Benites <arthurbenites@protonmail.com>
2022-05-07 09:59:00 -0300

    Refactoring commands

--- commands/messages.py
+++ commands/messages.py
@@ -1,28 +1,16 @@
 from discord.ext import commands
-from embeds import embed_weather_current, embed_weather_forecast
+from embeds import embed_weather_current
 
 
 class Messages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="clima", help="Não receber parâmetros")
-    async def send_current_weather(self, ctx, *city):
-        city = ' '.join(city)
-        embed_weather, file = embed_weather_current(self.bot,
-                                                    city)
-        await ctx.channel.send(file=file, embed=embed_weather)
+    @commands.command(name="BR", help="XXXX")
+    async def send_br_weather(self, ctx, estate, *city):
+        pass
 
-    @commands.command(name="previsao", help="""
-    Parâmetros: amanha, depois_amanha""")
-    async def send_tomorrow_weather(self, ctx, message):
-        if message in ["amanha", "depois_amanha"]:
-            embed_weather = embed_weather_forecast(self.bot, message)
-            await ctx.channel.send(embed=embed_weather)
-        else:
-            await ctx.channel.send("Parâmetro inválido. !!help para ajuda")
-
-    @commands.command(name="RS", help="XXX")
+    @commands.command(name="RS", help="XXXX")
     async def send_rs_weather(self, ctx, *city):
         try:
             city = ' '.join(city).title()
